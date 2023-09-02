dfe1a015fffcb48f3b390f731d4c362ed7d1a82d
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-09-02 14:57:15 +0300

    lesson1

---
+++ js/lessons-1.js
@@ -0,0 +1,8 @@
+// ЗАДАЧА 3
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+let priceTv = Number(prompt("Ввндіть ціну за телевізор."));
+let pricePhone = +prompt("Ввндіть ціну за телефон.");
+let priceRadio = +prompt("Ввндіть ціну за радіо.");
+
+console.log(priceTv + pricePhone + priceRadio);
